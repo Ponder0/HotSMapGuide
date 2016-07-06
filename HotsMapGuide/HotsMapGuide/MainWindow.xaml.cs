7ee2c5c48ac8258b4cb06a019a0883a8c1045284
Ponder0 <rltims0@gmail.com>
2016-07-06 03:17:21 -0400

    Refactoring and more work on UI. Starting working on timer idea.

--- HotsMapGuide/HotsMapGuide/MainWindow.xaml.cs
+++ HotsMapGuide/HotsMapGuide/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SQLite;
 
+using System.Timers;
+
 
 
 namespace HotsMapGuide
@@ -23,47 +25,90 @@ namespace HotsMapGuide
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Variables
+
+        // Constants
+        public const int EVENT_INFO_COLUMN = 2;
+        
         // SQLite connection
         SQLiteConnection dataConnection;
 
+        // Current Selection Variables
+        public string selectedMap = "";
+        public int currentStage = 1;
 
+        // Timer
+        Timer stageTimer = new Timer();
+
+        #endregion
+
+
+        // On Startup
         public MainWindow()
         {
             InitializeComponent();
 
-            // Set label text
-            label_MapName.Content = "HotS Map Guide";
-            label_EventInfo.Content = "Data to come...";
 
-            // Load data on startup
-            OpenDatabaseConnection();
+            // Load comboBox data on startup
             PopulateComboBoxWithMapNames();
-            CloseDatabaseConnection();
         }
 
-        
+
+        #region UI Updates
+
+        /// <summary>
+        /// Update UI Elements based on currently selected map
+        /// </summary>
+        public void UpdateUIElements()
+        {
+            label_MapName.Content = selectedMap;
+            UpdateEventInfoLabel();
+        }
+
 
         /// <summary>
         /// Fill Map Selector ComboBox with Map Names
         /// </summary>
         public void PopulateComboBoxWithMapNames()
         {
-            string query = "SELECT name FROM sqlite_master WHERE type='table'"; // Select names of all tables in database
-            SQLiteCommand createCommand = new SQLiteCommand(query, dataConnection);
-            SQLiteDataReader dataReader = createCommand.ExecuteReader();
+            OpenDatabaseConnection();
+
+            // Select names of all tables in database
+            SQLiteDataReader dataReader =
+                SendQueryAndReturnData("SELECT name FROM sqlite_master WHERE type='table'");
 
             while (dataReader.Read())
             {
                 // Add table names to comboBox
                 comboBox_MapSelector.Items.Add(dataReader.GetString(0));
             }
+
+            CloseDatabaseConnection();
         }
 
 
+        /// <summary>
+        /// Updates Event Info label from database
+        /// </summary>
+        public void UpdateEventInfoLabel()
+        {
+            OpenDatabaseConnection();
 
-        /**************
-         * Database Connection Helper Methods
-         **************/
+            SQLiteDataReader dataReader =
+                SendQueryAndReturnData("SELECT * FROM " + selectedMap + " WHERE ID=" + currentStage);
+
+            while (dataReader.Read())
+            {
+                label_EventInfo.Content = dataReader.GetString(EVENT_INFO_COLUMN); 
+            }
+
+            CloseDatabaseConnection();
+        }
+
+        #endregion
+
+
+        #region Database Connection Methods
 
         public void OpenDatabaseConnection()
         {
@@ -76,7 +121,73 @@ namespace HotsMapGuide
             dataConnection.Close();
         }
 
+        /// <summary>
+        /// Takes in and sends a SQL query then returns the Data Reader
+        /// </summary>
+        /// <param name="query">Query to the database</param>
+        /// <returns></returns>
+        public SQLiteDataReader SendQueryAndReturnData(string query)
+        {
+            SQLiteCommand createCommand = new SQLiteCommand(query, dataConnection);
+            SQLiteDataReader dataReader = createCommand.ExecuteReader();
+            return dataReader;
+        }
 
+        #endregion
+
+
+        #region UI Events
+
+        /// <summary>
+        /// Called when comboBox is set/changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox_MapSelector_DropDownClosed(object sender, EventArgs e)
+        {
+            // Set selected map from comboBox current selection
+            selectedMap = comboBox_MapSelector.SelectedItem.ToString();
+
+            UpdateUIElements();
+        }
+
+
+        /// <summary>
+        /// Called when the Start button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Start_Click(object sender, RoutedEventArgs e)
+        {
+            //StartTimer();
+        }
+
+        #endregion
+
+
+        /* Testing timer ideas
+        public void StartTimer()
+        {
+            //stageTimer.Elapsed += new ElapsedEventHandler(TestEvent);
+            //stageTimer.Interval = 60000;
+            //stageTimer.Enabled = true;
+
+            System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+
+            timer.Start();
+
+            // update progress bar
+            while (timer.IsRunning)
+            {
+                progressBar.Value = timer.ElapsedMilliseconds / 10000;
+            }
+        }
+        */
+
+        private static void TestEvent(object source, ElapsedEventArgs e)
+        {
+
+        }
     }
 
     
