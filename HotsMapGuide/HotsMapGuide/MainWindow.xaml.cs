eba822ce39853763fabd68da7af4534752730f54
Ponder0 <rltims0@gmail.com>
2016-07-06 01:40:19 -0400

    Added a comboBox for selecting the current map. Added a database file with map info and populated ComboBox with map names based on database tables.

--- HotsMapGuide/HotsMapGuide/MainWindow.xaml.cs
+++ HotsMapGuide/HotsMapGuide/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
+
+
 
 namespace HotsMapGuide
 {
@@ -20,13 +23,61 @@ namespace HotsMapGuide
     /// </summary>
     public partial class MainWindow : Window
     {
+        // SQLite connection
+        SQLiteConnection dataConnection;
+
+
         public MainWindow()
         {
             InitializeComponent();
 
             // Set label text
-            label.Content = "HotS Map Guide";
-            label2.Content = "Data to come...";
+            label_MapName.Content = "HotS Map Guide";
+            label_EventInfo.Content = "Data to come...";
+
+            // Load data on startup
+            OpenDatabaseConnection();
+            PopulateComboBoxWithMapNames();
+            CloseDatabaseConnection();
         }
+
+        
+
+        /// <summary>
+        /// Fill Map Selector ComboBox with Map Names
+        /// </summary>
+        public void PopulateComboBoxWithMapNames()
+        {
+            string query = "SELECT name FROM sqlite_master WHERE type='table'"; // Select names of all tables in database
+            SQLiteCommand createCommand = new SQLiteCommand(query, dataConnection);
+            SQLiteDataReader dataReader = createCommand.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                // Add table names to comboBox
+                comboBox_MapSelector.Items.Add(dataReader.GetString(0));
+            }
+        }
+
+
+
+        /**************
+         * Database Connection Helper Methods
+         **************/
+
+        public void OpenDatabaseConnection()
+        {
+            dataConnection = new SQLiteConnection(@"Data Source=C:\Users\LT\Source\Repos\HotSMapGuide\HotsMapGuide\HotsMapGuide\HotsMapsDB.db;Version=3;");
+            dataConnection.Open();
+        }
+
+        public void CloseDatabaseConnection()
+        {
+            dataConnection.Close();
+        }
+
+
     }
+
+    
 }
